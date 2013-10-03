a7cb69ca4cb9b264b8319fb8c442c6965f3ce8d9
geekfreak <geekfreak@gmail.com>
2013-10-03 21:25:16 +0200

    initial commit

---
+++ javascript/projects.js
@@ -0,0 +1,247 @@
+projects = {
+blank : {
+		ttle 		: "",
+		client		: "",
+		exturl		: "",
+		exturl_title: "",
+		cstudy_aref	: "",
+		cstudy_title: "",
+		imges		: ["","",""],
+		inotes		: ""
+		},
+
+bofa: {
+		ttle 		: "BANK OF AMERICA",
+		client		: "MBNA, International Websites",
+		exturl		: "http://www.mbna.ie/#TB_inline?TB_iframe=true&height=600&width=800",
+		exturl_title: "Bank of America/MBNA - http://www.mbna.ie, http://www.mbna.co.uk, http://www.mbna.es",
+		cstudy_aref	: "",
+		cstudy_title: "",
+		imges		: ["bofa7","bofa5","bofa8","bofa4"],
+		imgnotes	: ["MBNA Ireland, Homepage","Bank of America, International Sites","MBNA Spain, Compare Cards Wireframe ","MBNA UK, Card Collection Page"],
+		inotes		: "<p class='inotestext'>Redesign of Bank of America's three European acquisition websites, located in the UK, Ireland and Spain. Bank of America best practices and style guidelines were leveraged to create the sites and establish Bank of America as a global brand.</p><p class='inotestext'> SERVICES: Interaction design and information architecture. Documentation available on request.</p>"
+		},
+		
+rbofa: {
+		ttle 		: "BANK OF AMERICA",
+		client		: "Relationship Rewards, Web UI",
+		exturl		: "",
+		exturl_title: "",
+		cstudy_aref	: "",
+		cstudy_title: "",
+		imges		: ["rbofa1","rbofa2","rbofa4","rbofa3"],
+		imgnotes	: ["Rewards, Detailed History Page (Corporation)","Rewards, Detailed History Wireframe","Rewards, Process Flow","Rewards, Detailed History Page (Corporation Individual)"],
+		inotes		: "<p class='inotestext'>Design of a Relationship Rewards UI which allows customers to view detailed points history, redeem points for products or cash, and move points between products. Value segments are utilized to target customers with promotions.</p><p class='inotestext'>SERVICES: Interaction Design and UX project lead. Documentation available on request.</p>"
+		},
+		
+		
+atom: {
+		ttle 		: "ATOM ENTERTAINMENT",
+		client		: "Addicting Games, Website",
+		exturl		: "http://www.addictinggames.com/#TB_inline?TB_iframe=true&height=600&width=800",
+		exturl_title: "Addicting Games - http://www.addictingGames.com",
+		cstudy_aref	: "",
+		cstudy_title: "Case Study - AAU Online Glossary",
+		imges		: ["atom1","atom2","atom3","atom4"],
+		imgnotes	: ["Navigation, Concept 1","Navigation, Concept 2","Navigation, Concept 3","Sign Up Page"],
+		inotes		: "<p class='inotestext'>Design concepts for the global navigation and sign up page for an online gaming website owned by Atom entertaninment an MTV company.</p><p class='inotestext'> SERVICES: Visual design.</p>"
+		},
+		
+lang: {
+		ttle 		: "NEW LANGTON ARTS",
+		client		: "Art Gallery, Website",
+		exturl		: "",
+		exturl_title: "",
+		cstudy_aref	: "",
+		cstudy_title: "",
+		imges		: ["lang1","lang2","lang3","lang4"],
+		imgnotes	: ["Form Interface","Events Listing Page","Event Information Page","Home Page"],
+		inotes		: "<p class='inotestext'>The New Langton staff needed a simple way to update and archive the exhibitions on their website. A simple form was created which allowed them to create and archive pages by inputing text.</p><p class='inotestext'>SERVICES: Client strategy, interaction design, XHTML and CSS.</p>"
+		},
+		
+		
+ssdp2: {
+		ttle 		: "SAIDA & SULLIVAN",
+		client		: "Architectural Firm, Website",
+		exturl		: "http://www.saidasullivan.com/#TB_inline?TB_iframe=true&height=480&width=850",
+		exturl_title: "Saida & Sullivan Design Partners - <a href=http://www.saidasullivan.com target=_blank> http://www.saidasullivan.com</a>",
+		cstudy_aref	: "",
+		cstudy_title: "",
+		imges		: ["wssdp1","wssdp2","wssdp3","wssdp4"],
+		imgnotes	: ["Home Page","Project Page","Project Page, Fuctional Template","Contact Page"],
+		inotes		: "<p class='inotestext'>Redesign of a website to meet the primary client need of having a site which they could update easily. The project brief also required a clean, simple web interface which reflected a contemporary architectural vocabulary.</p><p class='inotestext'> SERVICES: Client strategy, information architecture, interaction design, visual design, CSS & XHTML.</p>"
+		},
+
+aau: {
+		ttle 		: "AAU ONLINE",
+		client		: "E-learning Web Application",
+		exturl		: "http://online.academyart.edu/#TB_inline?TB_iframe=true&height=600&width=800",
+		exturl_title: "Academy of Art University Online - http://online.academyart.edu",
+		cstudy_aref	: "casestudy_glossary/index.html#TB_inline?TB_iframe=true&height=480&width=640",
+		cstudy_title: "Case Study - AAU Online Glossary",
+		imges		: ["waau1","waau2","waau3","waau4"],
+		imgnotes	: ["Glossary, Wireframe","Glossary, Wireframe","Glossary, Wireframe","Glossary, Wireframe"],
+		inotes		: "<p class='inotestext'>UI specifications for a glossary tool to be used for researching, studying and saving the vocabulary terms for online art & design classes. The tool can be customized for each class.</p><p class='inotestext'>SERVICES: Requirements, interaction design, wireframes, navigational models and flows.</p>"
+		},
+		
+aau2: {            
+        ttle 		: "AAU ONLINE",
+		client		: "E-learning Courses",
+		exturl		: "http://online.academyart.edu/#TB_inline?TB_iframe=true&height=600&width=800",
+		exturl_title: "Academy of Art University Online - http://online.academyart.edu",
+		cstudy_aref	: "",
+		cstudy_title: "",
+		imges		: ["aau1","aau2","aau3","aau4"],
+        imgnotes	: ["AAU Online Homepage","Fabric Selector, Interior Design Class"," Welded & Fabricated Sculpture, <a href=http://www.x92studios.com/casestudy_flash/weld.html>View Slideshow</a>","Textile Design Class, <a href=http://www.x92studios.com/casestudy_flash/textile.html>View Slideshow</a>"],
+		inotes	 	: "<p class='inotestext'>The Academy of Art University translated all its traditional on campus classes to online learning. Produced, in a two year period over thirty full semester online art & design courses based on instructor requirements and student learning needs.</p><p class='inotestext'> SERVICES: Project management, user interface design, visual design, LMS maintenance, HTML, Flash and other multimedia applications.</p>"
+		},
+
+artthink: {
+		ttle 		: "SFMOMA",
+		client		: "Artthink Prototype",
+		exturl		: "http://www.sfmoma.org/artthink/#TB_inline?TB_iframe=true&height=480&width=800",
+		exturl_title      : "San Francisco Museum of Modern Art - http://www.sfmoma.org/artthink",
+		cstudy_aref	: "casestudy_artthink/index.html#TB_inline?TB_iframe=true&height=480&width=640",
+		cstudy_title    : "Case Study - Artthink",
+		imges		: ["msoma1","msoma2","msoma3","msoma4"],
+		imgnotes	: ["Entry Point from SFMOMA Website","Site Map for Prototype","Prototype Interface","Prototype Interface"],
+		inotes		: "<p class='inotestext'>The curriculum prototype was developed to test the concept of developing online high school curriculum utilizing flash based learning modules taken from the “Making Sense of Modern Art” (MSOMA) program.</p><p class='inotestext'>SERVICES: Information architecture, navigational models, process flows, HTML, CSS, Javascript and Flash.</p>"
+		},
+		
+msmoma: {
+		ttle 		: "SFMOMA",
+		client		: "Interactive Features, Website & Kiosk",
+		exturl		: "http://www.sfmoma.org/msoma/#TB_inline?TB_iframe=true&height=480&width=640",
+		exturl_title: "Making Sense of Modern Art - http://www.sfmoma.org/msoma",
+		cstudy_aref	: "",
+		cstudy_title: "",
+		imges		: ["sfmoma1","sfmoma2","sfmoma4","sfmoma3"],
+		imgnotes	: ["Making Sense of Modern Art","Roy Design Series Exhibition","Roy Design Series Exhibition","Philip Guston Exhibition"],
+		inotes		: "<p class='inotestext'>Created new modules for the Making Sense of Modern Art program and interactive features for current exhibitions in the museum.</p><p class='inotestext'>SERVICES: Visual design and layout to style guidelines. Color correction, HTML and Flash.</p>"
+		},
+		
+		
+tattoo: {
+		ttle 		: "TATTOO MD",
+		client		: "Medi-Spa & Laser Clinic, Website",
+		exturl		: "http://www.tattoomdla.com/#TB_inline?TB_iframe=true&height=480&width=800",
+		exturl_title:  "Tattoo MD Laser Clinic - http://www.tattoomdla.com",
+		cstudy_aref	: "",
+		cstudy_title: "",
+		imges		: ["tmdla1","tmdla2","tmdla3","tmdla4"],
+		imgnotes	: ["Home Page Before (left) and After (right)","Wire Frame for Press Page Admin","Content Management System for Press Page","Layout for Product Page"],
+		inotes		: "<p class='inotestext'>The Tattoo MD clinic needed an easy way for their staff to update the press section of their website. A web based form was created to allow any member of staff to quickly and easily make updates.</p><p class='inotestext'> SERVICES: Client strategy, interaction design, visual Design, XHTML, CSS, Flash and Wordpress.</p>"
+		},
+		
+fixx: {
+		ttle 		: "DAILY FIXX",
+		client		: "Magazine, Website",
+		exturl		: "http://www.xray92.com/dailyfixx/#TB_inline?TB_iframe=true&height=680&width=800",
+		exturl_title: "Daily Fix Screenshot - http://www.xray92.com/dailyfixx",
+		cstudy_aref	: "",
+		cstudy_title: "",
+		imges		: ["dfixx1","dfixx2","dfixx3","dfixx4"],
+		imgnotes	: ["Home Page","Article Page","Advertising Banner","Logo"],
+		inotes		: "<p class='inotestext'>This website was designed to gain venture capital funding for an online women's magazine, a magazine that combined substantive content with a hip, irreverent attitude. The brief was to design a site that said 'style meets intellect'.</p><p class='inotestext'> SERVICES: Visual design, identity system, banner advertisement and illustration.</p>"
+		},
+		
+
+parms: {
+		ttle 		: "EMOTION STUDIOS",
+		client		: "Project Arms, DVD",
+		exturl		: "",
+		exturl_title: "",
+		cstudy_aref	: "",
+		cstudy_title: "",
+		imges		: ["parms1","parms2","parms3","parms4"],
+		imgnotes	: ["DVD Menu Screen","DVD Special Features Screen","DVD Subtitle Screen","DVD Extras Screen"],
+		inotes		: "<p class='inotestext'>Project Arms is a Japanese anime DVD series. Menus were designed with an animation playing along the left side and the episode navigation, episode viewer and specific scene navigation in the center. The submenus are easy to acess and screen load times are fast. </p><p class='inotestext'>SERVICES: Video capture, video editing, production artwork, Photoshop.</p>"
+		},
+		
+filmoids: {
+		ttle 		: "FILMOIDS",
+		client		: "Desktop Video Application",
+		exturl		: "http://www.xray92.com/filmoids/#TB_inline?TB_iframe=true&height=580&width=700",
+		exturl_title: "Filmoids - <a href=http://www.xray92.com/filmoids target=_blank> http://www.xray92.com/filmoids</a>",
+		cstudy_aref	: "",
+		cstudy_title: "",
+		imges		: ["filmoids4","filmoids5","filmoids6","filmoids7"],
+		imgnotes	: ["Mapping Sequences with Genomes","Breeding Sequences","Rendering Movies", "Exhibition Installation"],
+		inotes		: "<p class='inotestext'>Filmoids is an experimental desktop video application which uses genetic algorithims to map sequences to psuedo genomes and then breed them with other sequences to create short digital movies. Graduate thesis project. </p><p class='inotestext'> SERVICES: Conceptual design, interface design, video creation and production.</p>" 
+		},		
+
+rainbow: {
+		ttle 		: "RAINBOW SERPENT",
+		client		: "Identity System",
+		exturl		: "",
+		exturl_title      : "",
+		cstudy_aref	: "",
+		cstudy_title: "",
+		imges		: ["rainbow1","rainbow2","rainbow3","rainbow4"],
+		imgnotes	: ["Identity System","Business Card","Packaging","Stationery"],
+		inotes		: "<p class='inotestext'>Creation of an identity system for a Massage and Wellness clinic.</p><p class='inotestext'>SERVICES: Logo design, visual design.</p>"
+		},
+
+road: {
+		ttle 		: "ROAD & STREET",
+		client		: "Identity System",
+		exturl		: "",
+		exturl_title      : "",
+		cstudy_aref	: "",
+		cstudy_title: "",
+		imges		: ["roadstreet1","roadstreet2","roadstreet4","roadstreet3"],
+		imgnotes	: ["Logo","Home Page","Cover","Double Page Spread"],
+		inotes		: "<p class='inotestext'>Road & Street was the concept for a magazine aimed at the drivers of environmentally friendly vehicles.</p><p class='inotestext'> SERVICES: Logo design, visual design and photography.</p>"
+		},
+
+radaptive: {
+		ttle 		: "RADAPTIVE",
+		client		: "Identity System",
+		exturl		: "http://www.radaptive.com/#TB_inline?TB_iframe=true&height=680&width=800",
+		exturl_title: "Radaptive - http://www.radaptive.com",
+		cstudy_aref	: "",
+		cstudy_title: "",
+		imges		: ["radaptive1","radaptive2","radaptive3","radaptive4"],
+		imgnotes	: ["Home Page","Business Card","Home Page Comps","Home Page"],
+		inotes		: "<p class='inotestext'>Radaptive is a high-tech company offering an operations management solution.</p><p class='inotestext'> SERVICES: Logo design, visual design.</p>"
+		},
+
+genolog: {
+		ttle 		: "GENOLOG",
+		client		: "Identity System",
+		exturl		: "http://www.xray92.com/genolog/#TB_inline?TB_iframe=true&height=580&width=700",
+		exturl_title: "Genolog Archive - <a href=http://www.xray92.com/genolog/slash/index.html target=_blank>http://www.xray92.com/genolog/slash/index.html</a>",
+		cstudy_aref	: "",
+		cstudy_title: "",
+		imges		: ["genolog4","genolog1","genolog2","genolog3"],
+		imgnotes	: ["Logo & Icons","Home Page","Admin Page","Glossary Page"],
+		inotes		: "<p class='inotestext'>Genelog was an open source website for posting academic articles, community discussion and exhibition of genetic, evolutionary and generative artworks.</p><p class='inotestext'>SERVICES: Logo design, icon design, content creation and editing.</p>"
+		},
+
+lamamma: {
+        ttle 		: "LA MAMMA",
+		client		: "Identity System",
+		exturl		: "",
+		exturl_title      : "",
+		cstudy_aref	: "",
+		cstudy_title: "",
+		imges		: ["mamma4","mamma1","mamma3","mamma2"],
+		imgnotes	: ["Logo","Magazine Cover","Double Page Spread","Double Page Spread"],
+		inotes		: "<p class='inotestext'>Design for a magazine spread celebrating the designer Gaetano Pesce's Up Series of chairs.</p><p class='inotestext'> SERVICES: Logo design, page layout and photography.</p>"
+		},
+
+tunnel: {
+        ttle 		: "TUNNEL CLOTHING",
+		client		: "Identity System",
+		exturl		: "",
+		exturl_title      : "",
+		cstudy_aref	: "",
+		cstudy_title: "",
+		imges		: ["tunnel1","tunnel2","tunnel3","tunnel4"],
+		imgnotes	: ["Stationery 1","Stationery 2","Logo Comps","<a href=http://www.x92studios.com/casestudy_flash/tunnel.html>View Flash Banner </a>"],
+		inotes		: "<p class='inotestext'>Tunnel is a small boutique located in Munich, which carries unique and trendy clothing items.</p><p class='inotestext'>SERVICES: Logo design, Flash banner.</p>"
+		},
+
+end : { }
+
+};
